c21df6587c51764a121f3edf82347baeb71337ac
Tevin <38287265+saphby@users.noreply.github.com>
2026-05-18 20:46:43 +0300

    Add Input Generator For Algorithms

---
+++ main.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+#include <cctype>
+#include <vector>
+#include <array>
+#include <ctime>
+
+std::vector<int> inputGenerator(int size, bool isSort){
+    std::vector<int> container;
+    container.reserve(size);
+
+    if (isSort) {
+        for (int i = 0; i < size; i++) {
+            container.push_back(i + 1);           
+        }
+    } else {
+        for (int i = 0; i < size; i++) {
+            container.push_back(rand() % 101);
+            std::cout << container[i] << std::endl; 
+        }
+
+    }
+    return container;
+}
+
+int main() {
+    srand(time(0));
+    inputGenerator(5, true);
+    inputGenerator(5, false);
+    return 0;
+}
