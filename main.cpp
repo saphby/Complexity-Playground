81d7cfc77a48ae6be5b7babc30f72aa345b715b0
Tevin <38287265+saphby@users.noreply.github.com>
2026-06-07 09:00:46 +0300

    Add Bubble Sort Logic

--- main.cpp
+++ main.cpp
@@ -31,13 +31,25 @@ int linearSearch(int target, const std::vector<int>& data) {
     return -1;
 }
 
+void bubbleSort(std::vector<int>& data) {
+    for (size_t i = data.size() - 1; i > 0; i--) {
+        for(size_t j = 0; j < i; j++) {
+            if (data[j] > data[j+1]) {
+                int temp = data[j];
+                data[j] = data[j+1];
+                data[j+1] = temp;
+            }
+        }
+    }
+}
+
 int main() {
     srand(time(0));
-    std::vector<int> linearSearchInput = inputGenerator(10000000, true);
     long long totalTime = 0;
     for (int i = 0; i < 50; i++) {
+        std::vector<int> bubbleSortInput = inputGenerator(10000, false);
         auto startTime = std::chrono::steady_clock::now();
-        linearSearch(10000001, linearSearchInput);
+        bubbleSort(bubbleSortInput);
         auto endTime = std::chrono::steady_clock::now();
         auto duration = std::chrono::duration_cast<std::chrono::microseconds>(endTime - startTime).count();
         totalTime += duration;
