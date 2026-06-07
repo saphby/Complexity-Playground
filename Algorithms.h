9bfa26b29fb870515eeba442b6e7d134b3247f8f
Tevin <38287265+saphby@users.noreply.github.com>
2026-06-07 11:05:55 +0300

    Add Header Code

---
+++ Algorithms.h
@@ -0,0 +1,14 @@
+#ifndef ALGORITHMS_H
+#define ALGORITHMS_H
+
+#include <vector>
+
+std::vector<int> inputGenerator(int size, bool isSort);
+
+int linearSearch(int target, const std::vector<int>& data);
+int binarySearch(int target, const std::vector<int>& data);
+void bubbleSort(std::vector<int>& data);
+void mergeSort(std::vector<int>& data, int left, int right);
+void merge(std::vector<int>& data, int left, int mid, int right);
+
+#endif
